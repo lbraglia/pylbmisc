b91932a1ac353e38f3f9237fbf75a8025feb32aa
Luca Braglia <lbraglia@gmail.com>
2023-04-14 11:13:45 +0200

    autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,12 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src/'))
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,7 +20,12 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',   # generazione automatica api
+    'sphinx.ext.viewcode',  # aggiungi link ipertestuali al codice
+    'sphinx.ext.napoleon'   # supporta sintassi a-la numpy
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']

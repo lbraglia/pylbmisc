4d472980139a033c4522ac646e8ef2f4e3f1e24a
Luca Braglia <lbraglia@gmail.com>
2023-04-14 12:30:09 +0200

    update

--- src/pylb/experiments/sphinx.py
+++ src/pylb/experiments/sphinx.py
@@ -1,60 +1,39 @@
 def basicrst(arg1, arg2):
     """
-    Funzione per uso personale
 
-    =============
-    Formattazione
-    =============
-    
-    Primo paragrafo.
+    This is a title
+    ===============
 
-    Secondo paragrafo con elenco e testo:
+    This is a section
+    -----------------
+
     1. primo elemento elenco numerato *corsivo*;
     2. secondo elemento elenco numerato **grassetto**:
+
        * sottoelenco puntato 1 
        * sottoelenco puntato 2
+
     3. codice ``print(100)``
+
+
+    This is a subsection
+    ~~~~~~~~~~~~~~~~~~~~
 
     .. Questo è un commento.
 
+    Questo è un `link a una pagina <https://www.google.it/>`_
+
+    definizione1
+     Definition of the term, which must be indented
+
+    definizione2
+     Description.
+
+    Blocchi di codice sono introdotti da doppio due punti (e indentati)::
     
+        ls asd
+        sudo rm -rf /
 
-    Titolo
-    =====
-
-    Sezione
-    -------
-
-    sottosezione
-    ~~~~~~~~~~
-
-    Link
-    ====
-
-    `Link a una pagina <https://domain.invalid/>`_
-
-    Se un link deve essere ripetuto `google`_ e `google`_ conveiene
-    definirlo una volta sola.
-    
-    .. _google: https://google.it/
-
-    ========
-    Funzioni
-    ========
-
-    :param arg1: primo argomento 
-    :param arg2: secondo argomento **grassetto*.
-
-    :returns: sum of two elements passed
-
-    Example::
-
-       sphinxexp1(1, 2)
-       sphinxexp1(2, 3)       
-       
-    
-    >>> sphinxexp1(1, 2)
-    3
 
     """
     return arg1 + arg2

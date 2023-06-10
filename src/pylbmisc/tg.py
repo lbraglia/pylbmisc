81fe9695280209b4965240224099debcdde54795
Luca Braglia <lbraglia@gmail.com>
2023-06-10 10:58:08 +0200

    update

--- src/pylbmisc/tg.py
+++ src/pylbmisc/tg.py
@@ -1,8 +1,18 @@
-def chat2df(fpath):
-    '''
+"""
+Modulo con funzioni di utilità per Telegram
+"""
 
-    fpath: Path to a json export of a telegram chat
+import json
+import pandas as pd
+
+from pathlib import Path
+
+def chat2df(fpath):
+    '''Trasforma una chat json esportata dal clienti desktop in un DataFrame Pandas
+
+    fpath: str|Path to a json export of a telegram chat
     '''
+    fpath = Path(fpath)
     with fpath.open() as f:
         js = json.load(f)
         msg = js["messages"]

c3ac94fcf1767d22f3e8bab7d4d0c6197b2da418
Luca Braglia <lbraglia@gmail.com>
2023-06-23 11:36:16 +0200

    update

--- src/pylbmisc/dm.py
+++ src/pylbmisc/dm.py
@@ -20,8 +20,23 @@ def _verboser(f):
 
 
 # --------------- coercion workers ----------------------------------------
+def to_bool(x: _pd.Series):
+    """Coerce to boolean a pd.Series using astype
+
+    >>> to_bool(pd.Series([1,0,1,0]))
+    >>> to_bool(pd.Series(["","yes","no","boh"]))
+    """
+    return x.astype("bool", copy=True)
+
+
 def to_integer(x: _pd.Series):
-    """Coerce to integer a pd.Series (if possible)"""
+    """Coerce to integer a pd.Series (if possible)
+
+    >>> to_integer(pd.Series([1, 2, 3]))
+    >>> to_integer(pd.Series(["2001", "2011", "1999"]))
+    >>> to_integer(pd.Series(["1.1", "2,1", "asd"]))
+    >>> to_integer(pd.Series([1., 2., 3., 4., 5., 6.]))
+    """
     return _pd.to_numeric(x, errors='coerce', downcast='integer')
 
 
@@ -31,8 +46,112 @@ def to_numeric(x: _pd.Series):
 
 
 def to_datetime(x: _pd.Series):
-    """Coerce to a datetime a pd.Series"""
+    """Coerce to a datetime a pd.Series
+
+    >>> to_datetime(pd.Series([str(dt.datetime.now())] * 6))
+    """
     return _pd.to_datetime(x, errors='coerce')
+
+
+def to_date(x: _pd.Series):
+    """Coerce to a date a pd.Series
+
+    >>> to_date(pd.Series([str(dt.datetime.now())] * 6))
+    >>> to_date(pd.Series(["2020-01-02", "2021-01-01", "2022-01-02"] * 2))
+    """
+    return to_datetime(x).dt.floor("D")
+
+
+def to_categorical(x: _pd.Series, lower=True, categories=None):
+    """Coerce to categorical a pd.Series of strings, with blank values as missing
+
+    >>> to_categorical(pd.Series([1, 2., 1., 2, 3]))
+    >>> to_categorical(pd.Series(["AA", "sd", "asd", "aa", ""]))
+    """
+    # copy the series
+    s = x.copy()
+    # string preprocessing
+    if _pd.api.types.is_string_dtype(s):
+        # string: rm spaces
+        s = s.str.strip()
+        s[s == ""] = _pd.NA
+        # string: lowercase
+        if lower:
+            s = s.str.lower()
+    # categorical making
+    return _pd.Categorical(s, categories=categories)
+
+
+def to_noyes(x: _pd.Series):
+    """Coerce to no/yes a (string or numerical) pd.Series
+
+    >>> to_noyes(pd.Series(["","yes","no","boh", "si"]))
+    >>> to_noyes(pd.Series([0.,1.,2.,1.])) # using to_bool so if != 0 it's yes
+    """
+    s = x.copy()
+    # string preprocessing
+    if _pd.api.types.is_string_dtype(s):
+        # take the first letter that can be n(o), s(i) or y(es)
+        l0 = s.copy().str.strip().str.lower().str[0]
+        l0[l0 == 's'] = 'y'
+        l0 = l0.map({"0": "n", "1": "y"}) # for strings 0/1
+    else:
+        l0 = to_bool(s).map({False: 'n', True: 'y'})
+    return to_categorical(l0.map({"n": "no", "y": "yes"}),
+                          lower=False,
+                          categories = ['no', 'yes'])
+
+
+def to_sex(x: _pd.Series):
+    """Coerce to male/female a pd.Series of strings (Mm/Ff)
+
+    >>> to_sex(pd.Series(["","m","f"," m", "Fm"]))
+    """
+    if _pd.api.types.is_string_dtype(s):
+        s = x.copy()
+        # take the first letter (Mm/Ff)
+        l0 = s.str.strip().str.lower().str[0]
+        return to_categorical(l0.map({"m": "male", "f": "female"}),
+                              lower=False,
+                              categories = ['male', 'female'])
+    else:
+        raise ValueError("x must be a pd.Series of strings")
+
+
+
+
+def to_recist(x: _pd.Series):
+    """Coerce to recist categories a pd.Series of strings
+
+    >>> to_recist(pd.Series(["RC", "PD", "SD", "PR", "RP", "boh"]))
+    """
+    if _pd.api.types.is_string_dtype(x):
+        s = x.copy()
+        # rm spaces and uppercase and take the first two letters
+        s = s.str.strip().str.upper().str[:2]
+        # uniform italian to english
+        ita2eng = {"RC": "CR", "RP": "PR"}
+        s = s.replace(ita2eng)
+        categs = ["CR", "PR", "SD", "PD"]
+        return to_categorical(s, lower=False, categories=categs)
+    else:
+        raise ValueError("x must be a pd.Series of strings")
+
+
+def to_other_specify(x: _pd.Series):
+    """Try to polish a bit a free-text variable and create a categorical one
+
+    >>> x = pd.Series(["asd", "asd", "", "prova", "ciao", 3]+ ["bar"]*4)
+    >>> to_other_specify(x)
+    """
+    s = x.copy().astype("str")
+    s = s.str.strip().str.lower()
+    s[s==""] = _pd.NA
+    categs = list(s.value_counts().index) # categs ordered by decreasing counts
+    return to_categorical(s,
+                          lower=False,
+                          categories = categs)
+
 
 
 # --------------- coercion class ----------------------------------------

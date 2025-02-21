8a3211b46df85e87c446b487cfdc8c5e292450df
Luca Braglia <lbraglia@gmail.com>
2025-02-21 10:14:27 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,7 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sys
+from pathlib import Path
 
+sys.path.insert(0, str(Path('..', 'src').resolve()))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 

ac49241e15fa418ee25fb5d568e82f2bb526d91d
Luca Braglia <lbraglia@gmail.com>
2025-03-01 12:25:52 +0100

    update

--- src/pylbmisc/datasets/__init__.py
+++ src/pylbmisc/datasets/__init__.py
@@ -37,6 +37,7 @@ def load(fname: str = "beetles1.csv", **kwargs) -> _pd.DataFrame:
     >>> import pylbmisc as lb
     >>> lb.datasets.ls()
     >>> df = lb.datasets.load("laureati.csv")
+
     """
     return _pd.read_csv(_dataset_dir / fname, engine="python",
                         dtype_backend=_default_dtype_backend,

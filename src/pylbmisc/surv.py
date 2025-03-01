aa2780a000fb871713f58534f173179631f4b953
Luca Braglia <lbraglia@gmail.com>
2025-03-01 15:21:10 +0100

    update

--- src/pylbmisc/surv.py
+++ src/pylbmisc/surv.py
@@ -12,20 +12,20 @@ from lifelines import KaplanMeierFitter as _KaplanMeierFitter
 from lifelines.plotting import add_at_risk_counts as _add_at_risk_counts
 from lifelines.statistics import multivariate_logrank_test as _multivariate_logrank_test
 from lifelines.utils import qth_survival_times as _qth_survival_times
-from pylbmisc.dm import to_date as _to_date
 from pylbmisc.dm import to_integer as _to_integer
 from pylbmisc.stats import p_format as _p_format
 from warnings import warn as _warn
 
 
 def _estquant(fit, quantiles):
-    "Return estimates and quantiles of survival function with confidence intervals"
+    """Return estimates and quantiles of survival function with confidence
+    intervals"""
     estimates = _pd.concat([fit.survival_function_, fit.confidence_interval_],
-                           axis = "columns")
+                           axis="columns")
     quant = _qth_survival_times(quantiles, estimates)
     quant = quant.reset_index()
     quant.columns = ["Quantile", "Estimate", "Lower", "Upper"]
-    estimates = estimates.reset_index() # take time as variable
+    estimates = estimates.reset_index()  # take time as variable
     estimates.columns = ["time", "Estimate", "Lower", "Upper"]
     return estimates, quant
 
@@ -77,22 +77,22 @@ def km(time,
     if plot:
         fig, ax = _plt.subplots()
         plot_at_risk = bool(counts)
-    if group is None: #----------------------single curve -----------------------------
+    if group is None:  # --------------------single curve --------------------
         kmf = _KaplanMeierFitter()
-        df = _pd.DataFrame({"time": time, "status": status}).dropna() # handle
+        df = _pd.DataFrame({"time": time, "status": status}).dropna()  # handle
         fit = kmf.fit(df["time"], df["status"])
         estimates, quants = _estquant(fit, quantiles)
         if plot:
-            ax = fit.plot_survival_function(loc = xticks, ci_alpha = ci_alpha)
+            ax = fit.plot_survival_function(loc=xticks, ci_alpha=ci_alpha)
             ax.set_ylabel(ylab)
             ax.set_xlabel(xlab)
             # avoid legend for 1 group
             lgnd = ax.legend()
             lgnd.set_visible(False)
             if plot_at_risk:
-                _add_at_risk_counts(fit, labels = ["All"],
-                                   rows_to_show = counts,
-                                   ax = ax, fig = fig)
+                _add_at_risk_counts(fit, labels=["All"],
+                                    rows_to_show=counts,
+                                    ax=ax, fig=fig)
                 _plt.tight_layout()
             fig.show()
         return {
@@ -113,38 +113,43 @@ def km(time,
         fits = {}               # fits
         estimates = {}          # survival estimates
         quants = []             # quantiles
-        df = _pd.DataFrame({"time": time, "status": status, "group": group}).dropna() # handle
+        df = _pd.DataFrame({"time": time,
+                            "status": status,
+                            "group": group}).dropna()
         df["group"] = df.group.cat.remove_unused_categories()
         new_categs = df.group.cat.categories.to_list()
         if len(new_categs) < len(categs):
             removed_categ = set(categs) - set(new_categs)
             removed_categ_str = ", ".join(removed_categ)
-            msg = f"Some categories were removed due to missingness: {removed_categ_str}."
+            msg = "Some categories were removed due" \
+                f" to missingness: {removed_categ_str}."
             _warn(msg)
         for categ in new_categs:
-            kmf = _KaplanMeierFitter(label = ylab)
+            kmf = _KaplanMeierFitter(label=ylab)
             mask = df["group"] == categ
-            fits[categ] = f = kmf.fit(df.loc[mask, "time"], df.loc[mask, "status"], label = categ)
+            fits[categ] = f = kmf.fit(df.loc[mask, "time"],
+                                      df.loc[mask, "status"],
+                                      label=categ)
             e, q = _estquant(f, quantiles)
             estimates[categ] = e
             q.insert(0, "Group", categ)
             quants.append(q)
             if plot:
-                kmf.plot_survival_function(ax = ax,
-                                           loc = xticks,
-                                           ci_alpha = ci_alpha)
+                kmf.plot_survival_function(ax=ax,
+                                           loc=xticks,
+                                           ci_alpha=ci_alpha)
         quants = _pd.concat(quants)
         lr = _multivariate_logrank_test(df["time"], df["group"], df["status"])
         if plot:
             ax.set_ylabel(ylab)
             ax.set_xlabel(xlab)
-            lgnd = ax.legend(loc = "upper right")
+            lgnd = ax.legend(loc="upper right")
             # lgnd.set_visible(False)
 
             if plot_at_risk:
                 _add_at_risk_counts(*list(fits.values()),
-                                   rows_to_show = counts,
-                                   ax = ax, fig = fig)
+                                    rows_to_show=counts,
+                                    ax=ax, fig=fig)
                 _plt.tight_layout()
             if plot_logrank:
                 lr_string = (f"logr: {lr.test_statistic:.3f}, "
@@ -167,14 +172,14 @@ def _date_check(x: _pd.Series, name: str = "date", can_be_none: bool = False):
     if (x is None) and (not can_be_none):
         msg = f"{name} can't be None"
         raise Exception(msg)
-    allowed = _pd.api.types.is_datetime64_dtype(x) 
+    allowed = _pd.api.types.is_datetime64_dtype(x)
     if not allowed:
         msg = f"{name} must be an allowed type (numeric or datetime)"
-        raise Exception (msg)
+        raise Exception(msg)
 
 
 def _check_sequential_dates(x: _pd.DataFrame):
-    int_dates = x.apply(_to_integer, axis = 1)
+    int_dates = x.apply(_to_integer, axis=1)
     na_dates = x.isna()
     int_dates[na_dates] = _pd.NA
     not_sequential = (int_dates.diff(axis=1) < 0)
@@ -182,47 +187,54 @@ def _check_sequential_dates(x: _pd.DataFrame):
     if not_sequential_mask.any():
         msg = "Some dates are not sequential."
         _warn(msg)
-        with _pd.option_context("display.max_rows", None, "display.max_columns", None):
-            print(_pd.concat([x, not_sequential], axis = 1).loc[not_sequential_mask, :])
+        with _pd.option_context("display.max_rows", None,
+                                "display.max_columns", None):
+            print(_pd.concat([x, not_sequential],
+                             axis=1).loc[not_sequential_mask, :])
 
 
-def _check_negative_times(s, t, outcome):
+def _check_negative_times(s, t, outcome, verbose=True):
     status = s.copy()
     time = t.copy()
     neg_times = time < 0
     if neg_times.any():
-        msg = f"Some {outcome} times are < 0. Setting to NA both time and status below. Please fix the dates."
-        _warn(msg)
-        with _pd.option_context("display.max_rows", None, "display.max_columns", None):
-            print(_pd.DataFrame({"status": status, "time": time}).loc[neg_times, :])
         status[neg_times] = _pd.NA
         time[neg_times] = _pd.NA
+        if verbose:
+            msg = f"Some {outcome} times are < 0. Setting to NA" \
+                " both time and status below. Please fix the dates."
+            _warn(msg)
+            with _pd.option_context("display.max_rows", None,
+                                    "display.max_columns", None):
+                print(_pd.DataFrame({"status": status,
+                                     "time": time}).loc[neg_times, :])
     return status, time
 
 
-def tteep(start_date = None,
-          prog_date = None,
-          death_date = None,
-          last_fup = None,
-          ep=["os", "pfs", "ttp"]# ,
-          # comprisk=True
+def tteep(start_date=None,
+          prog_date=None,
+          death_date=None,
+          last_fup=None,
+          ep=["os", "pfs", "ttp"],
+          verbose=True
           ):
     """This function calculates common oncology time to event end-points
     (Overall Survival, Progression Free Survival, Time to Relapse).
 
-    Overall survival (OS) is computed as time from {start_date} to
-    {death_date}. Time of patients who did not experienced the event (with missing
-    {death_date}) is censored at the time of last follow up ({last_fup}).
+    Overall survival (OS) is computed as time from start_date to
+    death_date. Time of patients who did not experienced the event (with
+    missing death_date) is censored at the time of last follow up
+    (last_fup).
 
-    Progression free survival (PFS) is calculated as time from {start_date}
-    to {prog_date} or {death_date}, whichever comes first. Time of
-    patients who did not experienced any event (missing both {prog_date} and
-    {death_date}) is censored at the time of last follow up ({last_fup}).
+    Progression free survival (PFS) is calculated as time from start_date
+    to prog_date or death_date, whichever comes first. Time of
+    patients who did not experienced any event (missing both prog_date and
+    death_date) is censored at the time of last follow up (last_fup).
 
-    Time to progression (TTP) is computed as time from {start_date} to
-    {prog_date}. Time of patients who did not experienced the event
-    (missing {prog_date}) is censored at the time of last follow up
-    ({last_fup}) or death ({death_date}) whichever comes first.
+    Time to progression (TTP) is computed as time from start_date to
+    prog_date. Time of patients who did not experienced the event (missing
+    prog_date) is censored at the time of last follow up (last_fup) or death
+    (death_date) whichever comes first.
 
     Parameters
     ----------
@@ -239,14 +251,41 @@ def tteep(start_date = None,
 
     Examples
     --------
-    >>> # date input
-    >>> df = pd.DataFrame({"start_date" : lb.dm.to_date(pd.Series(["1900-01-01", "1900-01-01", "1900-01-01", "1900-01-01",        pd.NA, "1900-01-01", "1900-04-01", pd.NA])),
-    >>>                    "prog_date"  : lb.dm.to_date(pd.Series(["1900-03-01", "1900-03-01",        pd.NA,        pd.NA,        pd.NA,        pd.NA, "1900-02-01", pd.NA])),
-    >>>                    "death_date" : lb.dm.to_date(pd.Series(["1900-06-01",        pd.NA, "1900-06-01",        pd.NA,        pd.NA,        pd.NA, "1900-06-01", pd.NA])),
-    >>>                    "last_fup"   : lb.dm.to_date(pd.Series(["1900-06-01", "1900-12-31", "1900-06-01", "1900-12-31", "1900-12-31",        pd.NA, "1900-06-01", pd.NA]))
-    >>> })
+    >>> import pandas as pd
+    >>> from pylbmisc.dm import to_date
+    >>> df = pd.DataFrame({
+    ...    "start_date" : to_date(["1900-01-01", "1900-01-01", "1900-01-01",
+    ...                            "1900-01-01", pd.NA, "1900-01-01",
+    ...                            "1900-04-01", pd.NA]),
+    ...    "prog_date"  : to_date(["1900-03-01", "1900-03-01", pd.NA,
+    ...                            pd.NA, pd.NA, pd.NA, "1900-02-01", pd.NA]),
+    ...    "death_date" : to_date(["1900-06-01",  pd.NA, "1900-06-01",
+    ...                            pd.NA, pd.NA, pd.NA, "1900-06-01", pd.NA]),
+    ...    "last_fup"   : to_date(["1900-06-01", "1900-12-31", "1900-06-01",
+    ...                            "1900-12-31", "1900-12-31", pd.NA,
+    ...                            "1900-06-01", pd.NA])
+    ... })
     >>> print(df)
-    >>> tteep(df.start_date, df.prog_date, df.death_date, df.last_fup)
+      start_date  prog_date death_date   last_fup
+    0 1900-01-01 1900-03-01 1900-06-01 1900-06-01
+    1 1900-01-01 1900-03-01        NaT 1900-12-31
+    2 1900-01-01        NaT 1900-06-01 1900-06-01
+    3 1900-01-01        NaT        NaT 1900-12-31
+    4        NaT        NaT        NaT 1900-12-31
+    5 1900-01-01        NaT        NaT        NaT
+    6 1900-04-01 1900-02-01 1900-06-01 1900-06-01
+    7        NaT        NaT        NaT        NaT
+    >>> tteep(df.start_date, df.prog_date, df.death_date, df.last_fup,
+    ...       verbose=False)
+       os_status  os_time  pfs_status  pfs_time  ttp_status  ttp_time
+    0          1    151.0           1      59.0           1      59.0
+    1          0    364.0           1      59.0           1      59.0
+    2          1    151.0           1     151.0           0     151.0
+    3          0    364.0           0     364.0           0     364.0
+    4       <NA>      NaN        <NA>       NaN        <NA>       NaN
+    5       <NA>      NaN        <NA>       NaN        <NA>       NaN
+    6          1     61.0        <NA>       NaN        <NA>       NaN
+    7       <NA>      NaN        <NA>       NaN        <NA>       NaN
 
     Reference
     ----------
@@ -255,16 +294,18 @@ def tteep(start_date = None,
 
     """
 
-    _date_check(x = start_date, name = "start_date", can_be_none = False)
-    _date_check(x = last_fup, name = "last_fup", can_be_none = False)
-    _date_check(x = prog_date, name = "prog_date", can_be_none = True)
-    _date_check(x = death_date, name ="death_date", can_be_none = True)
+    _date_check(x=start_date, name="start_date", can_be_none=False)
+    _date_check(x=last_fup, name="last_fup", can_be_none=False)
+    _date_check(x=prog_date, name="prog_date", can_be_none=True)
+    _date_check(x=death_date, name="death_date", can_be_none=True)
 
     n = len(start_date)
 
     # Create indicator variables based on dates
-    prog = (~prog_date.isna()) if (prog_date is not None) else _pd.Series([_pd.NA] * n)
-    death = (~death_date.isna()) if (death_date is not None) else _pd.Series([_pd.NA] * n)
+    prog = (~prog_date.isna()) if (prog_date is not None) \
+        else _pd.Series([_pd.NA] * n)
+    death = (~death_date.isna()) if (death_date is not None) \
+        else _pd.Series([_pd.NA] * n)
 
     # dates dataframe for checks and other
     all_dates = _pd.DataFrame({
@@ -275,7 +316,8 @@ def tteep(start_date = None,
     })
 
     # Check sequential dates
-    _check_sequential_dates(all_dates)
+    if verbose:
+        _check_sequential_dates(all_dates)
 
     rval = {}
     if "os" in ep:
@@ -283,7 +325,8 @@ def tteep(start_date = None,
         os_last_date = _np.where(os_status, death_date, last_fup)
         os_time = (os_last_date - start_date).dt.days
         os_status[os_time.isna()] = _pd.NA
-        s, t = _check_negative_times(os_status, os_time, "OS")
+        s, t = _check_negative_times(os_status, os_time,
+                                     "OS", verbose=verbose)
         rval["os_status"] = s
         rval["os_time"] = t
 
@@ -293,7 +336,8 @@ def tteep(start_date = None,
         pfs_last_date = _np.where(pfs_status, min_prog_death, last_fup)
         pfs_time = (pfs_last_date - start_date).dt.days
         pfs_status[pfs_time.isna()] = _pd.NA
-        s, t = _check_negative_times(pfs_status, pfs_time, "PFS")
+        s, t = _check_negative_times(pfs_status, pfs_time,
+                                     "PFS", verbose=verbose)
         rval["pfs_status"] = s
         rval["pfs_time"] = t
 
@@ -303,59 +347,14 @@ def tteep(start_date = None,
         ttp_last_date = _np.where(ttp_status, prog_date, min_death_lfup)
         ttp_time = (ttp_last_date - start_date).dt.days
         ttp_status[ttp_time.isna()] = _pd.NA
-        s, t = _check_negative_times(ttp_status, ttp_time, "TTP")
+        s, t = _check_negative_times(ttp_status, ttp_time,
+                                     "TTP", verbose=verbose)
         rval["ttp_status"] = s
         rval["ttp_time"] = t
-
 
     return _pd.DataFrame(rval)
 
 
-
 if __name__ == "__main__":
-
-    df = _pd.DataFrame({
-        "start_date" : _to_date(_pd.Series(["1900-01-01", "1900-01-01", "1900-01-01", "1900-01-01",        _pd.NA, "1900-01-01", "1900-04-01", _pd.NA])),
-        "prog_date"  : _to_date(_pd.Series(["1900-03-01", "1900-03-01",        _pd.NA,        _pd.NA,        _pd.NA,        _pd.NA, "1900-05-01", _pd.NA])),
-        "death_date" : _to_date(_pd.Series(["1900-06-01",        _pd.NA, "1900-06-01",        _pd.NA,        _pd.NA,        _pd.NA, "1900-06-01", _pd.NA])),
-        "last_fup"   : _to_date(_pd.Series(["1900-06-01", "1900-12-31", "1900-06-01", "1900-12-31", "1900-12-31",        _pd.NA, "1900-06-01", _pd.NA]))
-    })
-
-    # some wrong insertions
-    df2 = _pd.DataFrame({
-        "start_date" : _to_date(_pd.Series(["1900-01-01", "1900-01-01", "1900-01-01", "1900-01-01",        _pd.NA, "1900-01-01", "1900-04-01", _pd.NA])),
-        "prog_date"  : _to_date(_pd.Series(["1899-03-01", "1900-03-01",        _pd.NA,        _pd.NA,        _pd.NA,        _pd.NA, "1900-02-01", _pd.NA])),
-        "death_date" : _to_date(_pd.Series(["1900-06-01",        _pd.NA, "1900-06-01",        _pd.NA,        _pd.NA,        _pd.NA, "1900-06-01", _pd.NA])),
-        "last_fup"   : _to_date(_pd.Series(["1900-06-01", "1900-12-31", "1900-06-01", "1900-12-31", "1900-12-31",        _pd.NA, "1900-06-01", _pd.NA]))
-    })
-
-
-    print(tteep(df.start_date, df.prog_date, df.death_date, df.last_fup)) # ok
-    print(tteep(df2.start_date, df2.prog_date, df2.death_date, df2.last_fup)) # some wrong
-
-
-
-
-# if False:
-
-#     # Competing risk endpoint
-#     if comprisk:
-#         all_dates2_n = all_dates.apply(lambda x: x.astype('int64') // 10**9)
-
-#         first_event_date = all_dates2_n.min(axis=1)
-#         first_event_time = (first_event_date - all_dates2_n['start_date'])
-
-#         first_event_status_labels = ['Progression', 'Death', 'FUP exit']
-#         first_event_status_codes = all_dates2_n.apply(lambda row: row.idxmin(), axis=1).map(lambda x: first_event_status_labels.index(x))
-
-#         first_event_status_codes[first_event_time.isna()] = _pd.NA
-
-#         neg_times = (first_event_time < 0)
-#         if neg_times.any():
-#             print("some first times are < 0")
-#             if sequential_strict:
-#                 first_event_time[neg_times] = _pd.NA
-#                 first_event_status_codes[neg_times] = _pd.NA
-
-#         rval_df["first_event_time"] = first_event_time
-#         rval_df["first_event_status"] = _pd.Categorical.from_codes(first_event_status_codes, categories=first_event_status_labels)
+    import doctest
+    doctest.testmod()
